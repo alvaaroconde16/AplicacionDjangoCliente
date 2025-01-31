7b2a36d3902ced6db30a336a4094fdbd6b528998
Alvaro Conde <alvaro.conde.soria.al@iespoligonosur.org>
2025-01-31 13:47:42 +0100

    Django clienteç

--- viajes/views.py
+++ viajes/views.py
@@ -22,8 +22,8 @@ def index(request):
 
 
 def reservas_lista_api(request):
-    headers = {'Authorization' : 'Bearer '+env('OAUTH2_ACCESS_TOKEN')}    
-    response = requests.get('http://127.0.0.1:8000/api/v1/reservas', headers=headers)
+    headers = {'Authorization' : 'Bearer '+env('OAUTH2_ACCESS_TOKEN_ADMIN')}    
+    response = requests.get('https://alvaroconde.pythonanywhere.com/api/v1/reservas', headers=headers)
     reservas = response.json()
     return render(request, 'reservas/reserva_api.html', {'reservas_mostrar':reservas})
         
@@ -31,27 +31,27 @@ def reservas_lista_api(request):
 
 def usuarios_lista_api(request):
     headers = {'Authorization': 'Bearer ' + env('OAUTH2_ACCESS_TOKEN_PROVEEDOR')} 
-    response = requests.get('http://127.0.0.1:8000/api/v1/usuarios', headers=headers)
+    response = requests.get('http://alvaroconde.pythonanywhere.com/api/v1/usuarios', headers=headers)
     usuarios = response.json()
     return render(request, 'usuarios/usuario_api.html', {'usuarios_mostrar': usuarios})
 
 
 def reservasMejoradas_lista_api(request):
     headers = {'Authorization': 'Bearer ' + env('OAUTH2_ACCESS_TOKEN_CLIENTE')} 
-    response = requests.get('http://127.0.0.1:8000/api/v1/reservasMejoradas', headers=headers)
+    response = requests.get('http://alvaroconde.pythonanywhere.com/api/v1/reservasMejoradas', headers=headers)
     reservas = response.json()
     return render(request, 'reservas/reservaMejorada_api.html', {'reservas_mostrar': reservas})
 
 
 def alojamientosMejorados_lista_api(request):
     headers = {'Authorization': 'Bearer ' + env('OAUTH2_ACCESS_TOKEN_ADMIN')} 
-    response = requests.get('http://127.0.0.1:8000/api/v1/alojamientosMejorados', headers=headers)
+    response = requests.get('http://alvaroconde.pythonanywhere.com/api/v1/alojamientosMejorados', headers=headers)
     alojamientos = response.json()
     return render(request, 'alojamientos/alojamientoMejorado_api.html', {'alojamientos_mostrar': alojamientos})
 
 
 def transportesMejorados_lista_api(request):
     headers = {'Authorization': 'Bearer ' + env('OAUTH2_ACCESS_TOKEN_ADMIN')} 
-    response = requests.get('http://127.0.0.1:8000/api/v1/transportesMejorados', headers=headers)
+    response = requests.get('http://alvaroconde.pythonanywhere.com/api/v1/transportesMejorados', headers=headers)
     transportes = response.json()
     return render(request, 'transportes/transporteMejorado_api.html', {'transportes_mostrar': transportes})
